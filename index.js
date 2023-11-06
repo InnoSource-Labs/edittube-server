24326fd656465673c328471191591e03c4a98479
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-06 18:38:56 +0530

    feat: Added file upload to local disk

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const multer = require("multer");
+
+const save = multer({ dest: "uploads/" });
 const uploadVideo = require("./upload")
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
+app.post("/", save.array("videoFile"), (req, res) => {
     try {
-        uploadVideo.uploadVideo("Hello", "World", "TEST")
+        console.log(req.files)
+        return res
+        // uploadVideo.uploadVideo("Hello", "World", "TEST")
     }
     catch(err) {
         console.error(err)
