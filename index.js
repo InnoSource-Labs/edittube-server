2bd6d43abeb9eb7dce69909dbb48898f402ab40d
kuldeep <kuldeeprathore74@gmail.com>
2023-11-07 16:18:05 +0530

    mongo connect

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const multer = require("multer");
 const Stream = require("stream")
@@ -5,11 +6,21 @@ const Stream = require("stream")
 const save = multer();
 const uploadVideo = require("./upload")
 
+const mongoose = require("mongoose")
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 8000;
-
+const DB_URL = process.env.atlas_URL
+mongoose.connect(DB_URL)
+const dbConnect = mongoose.connection
+dbConnect.once("open",()=>{
+    console.log("successfull connected to database");
+})
+dbConnect.on("error",()=>{
+    console.log("failed to connnect to  database");
+})
 app.post("/", save.array("videoFile"), (req, res) => {
     try {
         const file = req.files?.[0]?.buffer;
