d7f87210ab35719d70a43c1c3f9bf5203c427008
Patel Divyesh <pateldivyesh1323@gmail.com>
2023-11-08 23:30:01 +0530

    Added cloudinary upload with buffer

--- routes/cloudinaryRoutes.js
+++ routes/cloudinaryRoutes.js
@@ -1,6 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 const dotenv = require("dotenv");
 const express = require("express")
+const multer = require("multer");
+const streamifier = require("streamifier");
+const save = multer();
 
 const router = express.Router();
 
@@ -13,29 +16,29 @@ cloudinary.config({
     secure: true
 });
 
-router.route('/').post(async (req, res) => {
+router.post('/', save.array("videoFile"), async (req, res) => {
     try {
-        const file = "./bunny.mp4"
-        const videoRes = await cloudinary.uploader.upload(file,
-            {
-                resource_type: "video",
-                folder: "edittube_videos"
-            }
-        )
-        res.status(200).json({ success: true, message: "Uploaded video successfully", data: videoRes });
+        const videoFile = req.files?.[0]?.buffer;
+        let cld_upload_stream = cloudinary.uploader.upload_stream({ folder: "edittube_videos", resource_type: "video" }, function (error, result) {
+            res.status(200).json({ success: true, message: "Video uploaded successfully", data: result });
+        });
+        streamifier.createReadStream(videoFile).pipe(cld_upload_stream);
     }
     catch (error) {
         res.status(500).json({ success: false, message: error })
     }
 })
 
-router.route('/').delete(async (req, res) => {
+router.delete('/', async (req, res) => {
     try {
-        const publicId = "edittube_videos/g3gy3rnfhmcmxaprkqyh"
+        const { publicId } = req.body;
+        if (!publicId) {
+            res.status(400).json({ success: false, message: "Please provide publicId" })
+        }
         const deleteRes = await cloudinary.uploader.destroy(publicId, {
             resource_type: "video"
         });
-        res.status(200).json({ success: true, message: "Deleted asset successfully", data: deleteRes })
+        res.status(200).json({ success: true, message: "Video deleted successfully", data: deleteRes })
     }
     catch (error) {
         res.status(500).json({ success: false, message: error })
