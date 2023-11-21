541d3cc59946a3a2dfdde67020ac6041409581e3
amitabhsahu <amitabh.sahu0008@gmail.com>
2023-11-21 13:51:21 +0530

    added API for logedin to update user on DB

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
+const { updateLogedinUser } = require("../../controllers/user");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Hi from /users router.");
+router.post("/logedin", async (req, res) => {
+    try {
+        const { uid, email } = req.body;
+        if (uid && email) {
+            const { status, user } = await updateLogedinUser(req.body);
+            res.status(status).json(user);
+        } else {
+            res.status(400).json({ message: "Bad Request" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 module.exports = router;
